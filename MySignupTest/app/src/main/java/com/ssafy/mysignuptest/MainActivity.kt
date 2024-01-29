6f7cc8c049141f6cc4f3370fa519b18f0ccdc8b1
JIWON CHOI <92070555+icefoxand@users.noreply.github.com>
2024-01-29 14:37:02 +0900

    .

--- MySignupTest/app/src/main/java/com/ssafy/mysignuptest/MainActivity.kt
+++ MySignupTest/app/src/main/java/com/ssafy/mysignuptest/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ssafy.mysignuptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_dup_id = findViewById<Button>(R.id.btn_dup_id)
+
+        btn_dup_id.setOnClickListener { it ->
+            Toast.makeText(this, "ID 중복검사 성공", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+
+
+
+// btn1.setOnClickListener { v ->
+// Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show()
+// }
